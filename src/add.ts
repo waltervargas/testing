093328134b8baf1bd962700d1134c1b0e37fc08e
Walter Vargas <w@walter.bio>
2025-08-19 17:49:44 +0200

    Add gitignore

--- src/add.ts
+++ src/add.ts
@@ -1,5 +1,22 @@
 function addTwoNum(a: number, b: number): number { 
-    return a + b;
+    if (checkA(a) && checkB(b))  {
+        return a + b;
+    }
+    return 0;
+}
+
+function checkA(a: number): boolean {
+    if (a > 0 && a < 100){
+        return true
+    }
+    return false
+}
+
+function checkB(b: number): boolean {
+    if (b > 0 && b < 100){
+        return true
+    }
+    return false
 }
 
 export { addTwoNum }
